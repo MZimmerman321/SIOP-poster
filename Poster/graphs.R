30d34b061d7511da773ca0da0e15c7a1cfc637ec
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2020-06-01 14:56:27 -0400

    Make some tweaks

--- Poster/graphs.R
+++ Poster/graphs.R
@@ -1,11 +1,23 @@
 # Library
-#library(tidyverse)
+library(tidyverse)
 #library(ggplot2)
 #library(dplyr)
-#library(hrbrthemes)
+library(hrbrthemes)
 
-#library(readxl)
-#X_plot_data <- read_excel("mz_poster_files/plot_data.xlsx")
+library(readxl)
+X_plot_data <- read_excel("mz_poster_files/plot_data.xlsx") %>% 
+  slice(1:6) %>% 
+  rename(i_corr = interest_corr,
+         i_min = i_ci_min,
+         i_max = i_ci_max,
+         se_corr = Int_se_corr,
+         se_min = i_se_ci_min,
+         se_max = i_se_ci_max) %>% 
+  pivot_longer(cols = -holland_codes,
+               names_to = c("type", ".value"),
+               names_sep = "_") %>% 
+  mutate(holland_codes = as_factor(holland_codes),
+         type = as_factor(type)) 
 #View(X_plot_data)
 
 #fix <- na.omit(X_plot_data)
@@ -13,30 +25,31 @@
   
 #lolipop_guild <-
   
-  fix %>% 
-ggplot() +
-  geom_point(aes(  x = holland_codes, 
-                   y = interest_corr,
-               color = "blue",
-                size = 10,)) +
-    geom_text(aes(x = holland_codes, color = "blue", fontface = "bold",
-               y = interest_corr,
-               label = interest_corr),
-              nudge_x = 0.2) +
-  geom_point(aes(  x = holland_codes, 
-                   y = Int_se_corr,  
-               color = "red",
-                size = 10)) +
-    geom_text(aes(x = holland_codes,  color = "red", fontface = "bold",
-                  y = Int_se_corr,
-                  label = Int_se_corr),
-              nudge_x = 0.2) +
-    geom_errorbar(aes(x = holland_codes, 
-                   ymin = i_ci_min , 
-                  ymax = i_ci_max, 
-                   alpha = .5,
-                   color = "blue"),
-                size = 3) +
+X_plot_data %>% 
+ggplot(
+  aes(
+    x = holland_codes,
+    y = corr,
+    color = holland_codes,
+    shape = type)
+) +
+  geom_point(
+    size = 10,
+    position = position_dodge(width = .7)) +
+  geom_errorbar(aes(ymin = min,
+                    ymax = max),
+                position = position_dodge(width = .7),
+                width = 0) +
+  scale_shape_manual(labels = c("I", "E"),
+                     values = c("I", "E")) + 
+  coord_flip() +
+  scale_y_continuous(limits = c(.5, 1)) + 
+  scale_x_discrete(
+    limits = rev(levels(X_plot_data$holland_codes))
+    ) +
+  psychmeta:::theme_apa_nolegend
+
+    
     geom_errorbar(aes(x = holland_codes, 
                    ymin = i_se_ci_min, 
                    ymax = i_se_ci_max, 
@@ -44,7 +57,14 @@ ggplot() +
                    alpha = .5),
                size = 3) +
   coord_flip() +
-  theme_minimal() +
+  labs(title = "Convergent correlations between measures of interests with other 
+       interest measures (red) and with self-efficacy measures (blue).") +
+  xlab("") +
+  ylab("Correlations with confidance Intervals") +
+  theme_minimal() 
+
+
++
   theme(legend.position = "none",
          panel.background = element_rect(fill = "transparent"), 
           plot.background = element_rect(fill = "transparent", color = NA), 
@@ -54,11 +74,7 @@ ggplot() +
     legend.box.background = element_rect(fill = "transparent"),
     text = element_text(size = 15),
     plot.title = element_text(hjust = 2)
-  ) +
-  labs(title = "Convergent correlations between measures of interests with other 
-       interest measures (red) and with self-efficacy measures (blue).") +
-  xlab("") +
-  ylab("Correlations with confidance Intervals")
+  ) 
 
 ggsave(here::here("images", "lolipop_guild.jpg"))
 
